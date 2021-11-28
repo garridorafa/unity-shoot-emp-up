60720df48ac545d036338220ae42b1e23290490c
Rafa Garrido <rafagarridolara@gmail.com>
2021-11-28 00:06:39 -0400

    Add background

---
+++ Assets/Scripts/AutoScroller.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroller : MonoBehaviour
+{
+    public float scrollSpeed = 0.05f;
+    [SerializeField] private MeshRenderer mesh;
+
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * scrollSpeed);
+        mesh.material.mainTextureOffset = offset;
+    }
+}
