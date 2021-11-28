1d75ee53c59fc53fe9f45e8d9c3a738c2edd2181
Rafa Garrido <rafagarridolara@gmail.com>
2021-11-28 00:22:38 -0400

    Set boundary limits

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary {
+    public float xMin, xMax, yMin, yMax;
+}
+
 public class PlayerController : MonoBehaviour
 {
+    public Boundary boundary;
     public float speed = 1;
 
     void Update()
     {
         transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, transform.position.z);
+        
+        float x = Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax);
+        float y = Mathf.Clamp(transform.position.y, boundary.yMin, boundary.yMax);
+        transform.position = new Vector3(x, y);
     }
 }
