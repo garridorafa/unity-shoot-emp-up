fcd53f6b17aea86204aa3a6250ec2f4117c36d50
Rafa Garrido <rafagarridolara@gmail.com>
2021-12-01 23:13:29 -0400

    Create a parallax prefabs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Boundary {
-    public float xMin, xMax, yMin, yMax;
+public class Boundary
+{
+    public float xMinimun, xMaximun, yMinimun, yMaximun;
 }
 
 public class PlayerController : MonoBehaviour
 {
+    public Mover moverComponent;
     public Boundary boundary;
     public float speed = 1;
 
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, transform.position.z);
-        
-        float x = Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax);
-        float y = Mathf.Clamp(transform.position.y, boundary.yMin, boundary.yMax);
+        Vector3 displacement = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, transform.position.z);
+        moverComponent.DoMove(displacement);
+
+        float x = Mathf.Clamp(transform.position.x, boundary.xMinimun, boundary.xMaximun);
+        float y = Mathf.Clamp(transform.position.y, boundary.yMinimun, boundary.yMaximun);
         transform.position = new Vector3(x, y);
     }
 }
